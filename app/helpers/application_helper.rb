8ccd92ae27f74336da01df01d2938f3723064512
hugo.barrena <hugo.barrena@cespi.unlp.edu.ar>
2017-01-16 22:08:55 -0300

    Se definió un layout para la aplicación, se agregaron archivos de internacionalización, se definieron vistas compartidas y helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def icon(name, attributes = nil)
+    content_tag :i, '', (attributes || {}).merge(class: "glyphicon glyphicon-#{name}")
+  end
+
+  def boolean(value)
+    icon(!!value ? 'ok' : 'remove')
+  end
+
+  def render_value(value)
+    value.present? ? value : t('value.blank')
+  end
 end
