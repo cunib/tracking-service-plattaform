d4ee2b943533dceac9920fefc597a32f48b457bc
hugo.barrena <hugo.barrena@cespi.unlp.edu.ar>
2017-01-16 22:11:11 -0300

    Se creó el módulo de business.

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,50 @@
+class BusinessesController < ApplicationController
+  before_action :set_business, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @businesses = Business.all
+    @q = @businesses.search(session_params)
+    @q.sorts = ["created_at desc"]
+    @businesses = @q.result.page(page_param)
+    respond_with(@businesses)
+  end
+
+  def show
+    respond_with(@business)
+  end
+
+  def new
+    @business = Business.new
+    respond_with(@business)
+  end
+
+  def edit
+  end
+
+  def create
+    @business = Business.new(business_params)
+    @business.save
+    respond_with(@business)
+  end
+
+  def update
+    @business.update(business_params)
+    respond_with(@business)
+  end
+
+  def destroy
+    @business.destroy
+    respond_with(@business)
+  end
+
+  private
+    def set_business
+      @business = Business.find(params[:id])
+    end
+
+    def business_params
+      params.require(:business).permit(:name, :address)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161211235207) do
+
+  create_table "businesses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
