63940db01f704d0fad7a973f0e9c233f3c3dd2d9
hugo.barrena <hugo.barrena@cespi.unlp.edu.ar>
2017-01-27 14:00:20 -0300

    Se corrigió error en carga de bootstrap.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ujs
+//= require bootstrap
 //= require nprogress
 //= require nprogress-turbolinks
-//= require bootstrap-sprockets
+
