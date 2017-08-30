9cd849a879d892f54bb98eda568e385301ba1850
hugo.barrena <hugo.barrena@cespi.unlp.edu.ar>
2017-08-30 08:35:09 -0300

    Se seteó por defecto el monto del producto al asociar un producto a una orden.

--- app/models/ordered_product.rb
+++ app/models/ordered_product.rb
@@ -1,4 +1,12 @@
 class OrderedProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  before_validation :set_amount
+
+  private
+
+  def set_amount
+    self.amount = product.price
+  end
 end
