badd051910949b3fe837324b1f3cb20860f90679
hugo.barrena <hugo.barrena@cespi.unlp.edu.ar>
2017-09-16 14:57:34 -0300

    Se definió el serializer de producto para que se incluya en la respuesta de recorrido junto con las ordenes.

---
+++ app/serializers/product_serializer.rb
@@ -0,0 +1,6 @@
+class ProductSerializer < ActiveModel::Serializer
+  belongs_to :business
+  has_many :ordered_products
+  has_many :orders, through: :ordered_products
+  attributes :id, :name, :description, :price
+end
