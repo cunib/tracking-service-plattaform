f811f13cc40407a6175b27fb9ece60fa88930245
hugo.barrena <hugo.barrena@cespi.unlp.edu.ar>
2017-10-04 11:25:43 -0300

    Se refactorizaron serializers.

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -3,5 +3,6 @@ class OrderSerializer < ActiveModel::Serializer
   has_one :position
   has_one :delivery
   has_many :ordered_products
+  has_many :products
   attributes :id, :start_date, :end_date, :address, :status
 end
