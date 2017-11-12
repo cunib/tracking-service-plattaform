16b8f8ed2edf01cf816db6c0354302655f00b124
hugo.barrena <hugo.barrena@cespi.unlp.edu.ar>
2017-11-12 19:03:01 -0300

    Se agregaron atributos del comprador a los serializers de ordenes.

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -4,5 +4,5 @@ class OrderSerializer < ActiveModel::Serializer
   has_one :delivery
   has_many :ordered_products
   has_many :products
-  attributes :id, :start_date, :end_date, :address, :status
+  attributes :id, :start_date, :end_date, :address, :status, :customer_full_name, :customer_email
 end
--- app/serializers/orders_serializer.rb
+++ app/serializers/orders_serializer.rb
@@ -3,5 +3,5 @@ class OrdersSerializer < ActiveModel::Serializer
   belongs_to :position
   has_one :delivery
   has_many :ordered_products
-  attributes :id, :start_date, :end_date, :address, :status
+  attributes :id, :start_date, :end_date, :address, :status, :customer_full_name, :customer_email
 end
