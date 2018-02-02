2539f7217180894283fa8fafc8f5d95c8e4ec090
hugo.barrena <hugo.barrena@cespi.unlp.edu.ar>
2018-02-02 13:58:09 -0300

    Se corrigió estrategia de recorrido dijkstra.

--- app/models/concerns/dijkstra_path_strategy.rb
+++ app/models/concerns/dijkstra_path_strategy.rb
@@ -1,28 +1,32 @@
 class DijkstraPathStrategy < PathStrategy
- require 'node_with_distances'
+  require 'node_with_distances'
 
- attr_accessor :path, :total_distance, :next_node, :nodes, :distance_hash
+  attr_accessor :path, :total_distance, :next_node, :nodes, :distance_hash
 
- def initialize(source, nodes)
-   @nodes = nodes.unshift(source)
-   @path = [source]
-   @total_distance = 0
-   @next_node= source
-   @distance_hash = NodeWithDistances.new(@nodes).build_distance_hash
- end
+  def initialize(source, nodes)
+    @nodes = nodes.unshift(source)
+    @path = [source]
+    @total_distance = 0
+    @next_node= source
+    @distance_hash = NodeWithDistances.new(@nodes).build_distance_hash
+  end
 
   def build_path
+      next_posible_node = @next_node
     while @path.size < @nodes.size
+      min_distance = 999999
       @distance_hash[next_node].each do |node_to, distance|
-        min_distance = 999999
         unless @path.include?(node_to)
           if distance < min_distance
             min_distance = distance
-            @next_node = node_to
+            next_posible_node = node_to
           end
-          @total_distance = @total_distance + min_distance
-          @path << @next_node
         end
+      end
+      unless @path.include?(next_posible_node)
+        @next_node = next_posible_node
+        @total_distance = @total_distance + min_distance
+        @path << @next_node
       end
     end
 
